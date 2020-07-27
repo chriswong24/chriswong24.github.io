e796a81e517dcbfe9ae1f211ef5b984b0028a1a4
Christopher Wong <chwong@factset.com>
2020-07-26 21:57:42 -0400

    adding drop generator

---
+++ scripts/drop-generator.js
@@ -0,0 +1,21 @@
+function randomValFromArray(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+const numbers = [2,3,4,5,6,7,8];
+const letters = ['C','D','E','F','G','H'];
+
+const mapArr = [];
+
+
+// d1, e1, f1, i4, i5, i6, i7, i8
+const remainingSquares = [{number: 1, letter: 'D'}, {number: 1, letter: 'E'}, {number: 1, letter: 'F'}, {number: 4, letter: 'I'},
+                          {number: 5, letter: 'I'}, {number: 6, letter: 'I'}, {number: 7, letter: 'I'}, {number: 8, letter: 'I'}]; 
+
+
+numbers.forEach(num => letters.forEach(letter => mapArr.push({number: num, letter: letter})));
+remainingSquares.forEach(square => mapArr.push(square));
+
+const dropPoint = randomValFromArray(remainingSquares);
+
+document.getElementById('drop-point').innerHTML = dropPoint.letter + dropPoint.numnber;
